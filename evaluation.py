141493ae5488aeab72dc3c9aa65155026b4917b9
sjsachin <38081862+sjsachin@users.noreply.github.com>
2018-06-04 14:39:20 +0530

    Add files via upload

---
+++ evaluation.py
@@ -0,0 +1,27 @@
+import pickle
+import numpy as np
+import pandas as pd
+
+pickle_in=open("model_1_score.pickle","rb")
+model_1_score=pickle.load(pickle_in)
+
+pickle_in=open("model_2_score.pickle","rb")
+model_2_score=pickle.load(pickle_in)
+
+from datetime import datetime
+datestring = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+
+file = open('dataOn_'+datestring+'.txt', "w")
+
+if model_1_score>model_2_score:
+   file.write("model_1 is better ")
+   #file.write("model_1 score is")
+   #file.write(model_1_score)
+elif model_1_score < model_2_score:
+    file.write("model_2 is better ")
+    #file.write("model_2 score is")
+    #file.write(model_2_score)
+else:
+    file.write("Both model have equal accuracy")
+
+file.close()
