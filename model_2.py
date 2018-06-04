9ecf600ce212eca7d4dfda9a85f8d805805a2119
sjsachin <38081862+sjsachin@users.noreply.github.com>
2018-06-04 16:52:52 +0530

    Update model_2.py

--- model_2.py
+++ model_2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pickle
+from pandas import Series
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
